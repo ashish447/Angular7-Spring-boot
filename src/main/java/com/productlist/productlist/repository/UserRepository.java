afd9c95d3edcce82529b5adf4e0b03fd13c7b031
ashish447 <ashish.kasodariya@gmail.com>
2021-01-22 17:17:25 +0530

    coiitt

--- src/main/java/com/productlist/productlist/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.productlist.productlist.repository;
-
-import com.productlist.productlist.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}

d6404ca24d0645cfdfebc0aed65bc55f2705176b
ashish447 <ashish.kasodariya@gmail.com>
2021-01-23 23:07:26 +0530

    remove tsst

--- src/test/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootCrudRestfulWebservicesApplicationTests.java
@@ -3,10 +3,9 @@ package net.javaguides.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringbootCrudRestfulWebservicesApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
